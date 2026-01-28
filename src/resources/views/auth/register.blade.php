32928637ce48e34fdb7b411ade3dcfd21171ecbb
shiro <okano.03.remi@gmail.com>
2026-01-28 22:06:33 +0900

    Fortify-register/login

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+
+<nav>
+    <ul class="header-nav">
+        <li class="header-nav__item">
+            <a class="header-nav__link" href="href="/login">login</a>
+        </li>
+    </ul>
+</nav>
+
+
+@section('content')
+<div class="register-form__content">
+  <div class="register-form__heading">
+    <h2>Register</h2>
+  </div>
+  <form class="form">
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">お名前</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="text" name="name" value="{{ old('name') }}" />
+        </div>
+        <div class="form__error">
+          @error('name')
+          {{ $message }}
+          @enderror
+        </div>
+      </div>
+    </div>
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">メールアドレス</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="email" name="email" value="{{ old('email') }}" />
+        </div>
+        <div class="form__error">
+          @error('email')
+          {{ $message }}
+          @enderror
+        </div>
+      </div>
+    </div>
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">パスワード</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="password" name="password" />
+        </div>
+        <div class="form__error">
+          @error('password')
+          {{ $message }}
+          @enderror
+        </div>
+      </div>
+    </div>
+    <div class="form__button">
+      <button class="form__button-submit" type="submit">登録</button>
+    </div>
+  </form>
+</div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,19 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\RegisteredUserController;
-use App\Http\Controllers\AuthenticatedSessionController;
-
 
 Route::get('/', [ContactController::class, 'index'])->name('home');;
 Route::post('/confirm', [ContactController::class, 'confirm']);
 Route::post('/thanks', [ContactController::class, 'store']);
-
-
-
-Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::post('/register', [RegisteredUserController::class, 'store']);
-
-Route::get('/login', [AuthenticatedSessionController::class, 'create']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);

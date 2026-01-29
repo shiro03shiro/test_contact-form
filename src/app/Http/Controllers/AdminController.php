1f00b535881a34990cc91673c273a322b4e76c7f
shiro <okano.03.remi@gmail.com>
2026-01-30 02:07:05 +0900

    dash表示（仮）Admin作成

---
+++ src/app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('admin.dashboard', ['contacts' => $contacts]);
+    }
+}

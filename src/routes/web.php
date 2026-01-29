b8486a36a83c749c408fd6f615dcfcc15ae774a8
shiro <okano.03.remi@gmail.com>
2026-01-29 22:46:17 +0900

    /confirm_test

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index'])->name('home');
-Route::post('/confirm', [ContactController::class, 'confirm']);
+Route::post('/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
 Route::post('/thanks', [ContactController::class, 'store']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/login', [AuthController::class, 'login']);
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/login', [AuthController::class, 'login']);
+// });
